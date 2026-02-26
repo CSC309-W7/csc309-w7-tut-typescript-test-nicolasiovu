cbbc8c64d9bb64b918bc74da0b67a172a9cc79a4
iovunico <nicolas.iovu@mail.utoronto.ca>
2026-02-25 19:00:49 -0500

    done

--- src/users.ts
+++ src/users.ts
@@ -1,13 +1,21 @@
 import type { User } from "./types";
 
-export const apiResponse: unknown = [
+export const apiResponse: { name: string, age: number | string }[] = [
   { name: "Tony", age: 23 },
   { name: "Kevin", age: "24" }, // invalid
   { name: "Jim", age: 25 },
 ];
 
+function fixUser(user: { name: string, age: number | string }) : User {
+  const result: User = {
+    name: user.name,
+    age: Number(user.age)
+  };
+  return result;
+}
+
 export function getUsersData(): User[] {
-  return apiResponse as User[]; // intentionally unsafe
+  return apiResponse.map((u) => fixUser(u));
 }
 
 export function formatAges(users: User[]): string[] {
